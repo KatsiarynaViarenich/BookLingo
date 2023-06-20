c2c35b4d104724948ebf22d2617323f95b772879
Vera <veraword44@gmail.com>
2023-06-20 17:56:05 +0300

    )

--- gui/gui_main.py
+++ gui/gui_main.py
@@ -2,21 +2,38 @@ import sys
 
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QVBoxLayout, QWidget, \
     QListWidgetItem, QMessageBox
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from Page import Ui_PageMainWindow
 from Loged import Ui_LogedQMainWindow
 from NotLoged import Ui_NotLogedMainWindow
 from new_vision import Ui_MainWindow
 from LoginWindow import Ui_LoginPageMainWindow
 from RegisterWindow import Ui_RegisterQMainWindow
+from services.authorizing_process import AuthorizingProcess
+from services.user_session import UserSession
+from scripts.run_setup import User,Book,Quote,Base
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+
+        self.user = None
+        """
+        Is session must be inside? 
+        """
+        engine = create_engine('sqlite:///../data/app.db')
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        self.ap=AuthorizingProcess(session)
         self.ui=Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui_page=Ui_PageMainWindow()
         self.open_book() #wkinut w funkcyu
+
+
 
         self.ui_loged=Ui_LogedQMainWindow()
         self.ui_notloged=Ui_NotLogedMainWindow()
@@ -30,8 +47,13 @@ class MainWindow(QMainWindow):
         self.ui.tabWidget.setCurrentIndex(0)
 
     def open_tab(self, index):
-        if self.ui_log==self.ui_notloged:
-            if index not in [0, 1]:  # Индексы вкладок "Home" и "Account"
+        if self.ui_log == self.ui_logPage or self.ui_log == self.ui_register:
+            if index!=1:
+                QMessageBox.warning(self, "Oops...", "You're not logged.\nGo to Accounts.")
+                self.logOut()
+                self.ui.tabWidget.setCurrentIndex(0)
+        elif self.ui_log==self.ui_notloged:
+            if index not in [0, 1]:
                 QMessageBox.warning(self, "Oops...", "You're not logged.\nGo to Accounts.")
                 self.ui.tabWidget.setCurrentIndex(0)
             else:
@@ -44,47 +66,72 @@ class MainWindow(QMainWindow):
             self.logOut()
 
     def logInPage(self):
-        tab_index = 1  # Индекс целевой закладки
+        tab_index = 1
         self.ui.tabWidget.removeTab(tab_index)
         self.ui.tab_account=QWidget()
         self.ui.tabWidget.insertTab(tab_index,self.ui.tab_account,"Login")
         self.ui_log=self.ui_logPage
 
         self.ui_log.setupUi(self.ui.tab_account)
-        self.ui_log.LogInButton.clicked.connect(self.logIn)
+        self.ui_log.LogInButton.clicked.connect(self.authorization_login)
         self.ui_log.RegisterButton.clicked.connect(self.registerPage)
 
         self.ui.tabWidget.setCurrentIndex(tab_index)
 
-        #self.ui_log = self.ui_loged
-        #self.ui_log.setupUi(self.ui.tab_account)
-        #self.ui_log.LogOutButton.clicked.connect(self.logOut)
-        #self.ui.tabWidget.setCurrentIndex(tab_index)
+    def authorization_login(self):
+        #you're in loginpage
+        username=self.ui_log.loginLine.text()
+        password=self.ui_log.passLine.text()
+        message=self.ap.log_in(username,password)
+        QMessageBox.warning(self, "Authorizing", message)
+        if message!="Login successful":
+            self.logInPage()
+        else:
+            self.user = self.session.query(User).filter_by(name=username).first()
+            print(f"{username},{password}")
+            self.logIn()
 
+
+    def authorization_create_acc(self):
+        username=self.ui_log.loginLine.text()
+        password=self.ui_log.passLine.text()
+        message=self.ap.create_new_account(username,password)
+
+        if message!="Account created successfully":
+            QMessageBox.warning(self, "Authorizing", message)
+            self.registerPage()
+        else:
+            print(f"{username},{password}")
+            self.logIn()
 
     def registerPage(self):
-        tab_index = 1  # Индекс целевой закладки
+        tab_index = 1
         self.ui.tabWidget.removeTab(tab_index)
         self.ui.tab_account = QWidget()
         self.ui.tabWidget.insertTab(tab_index, self.ui.tab_account, "Register")
         self.ui_log = self.ui_register
 
         self.ui_log.setupUi(self.ui.tab_account)
-        self.ui_log.RegisterButton.clicked.connect(self.logOut)
+        self.ui_log.RegisterButton.clicked.connect(self.authorization_create_acc)
 
         self.ui.tabWidget.setCurrentIndex(tab_index)
 
 
     def logIn(self):
-        tab_index = 1  # Индекс целевой закладки
-        self.ui.tabWidget.removeTab(tab_index)
-        self.ui.tab_account = QWidget()
-        self.ui.tabWidget.insertTab(tab_index, self.ui.tab_account, "Account")
-        self.ui_log = self.ui_loged
-        self.ui_log.setupUi(self.ui.tab_account)
-        self.ui_log.LogOutButton.clicked.connect(self.logOut)
 
-        self.ui.tabWidget.setCurrentIndex(tab_index)
+            tab_index = 1
+            self.ui.tabWidget.removeTab(tab_index)
+            self.ui.tab_account = QWidget()
+            self.ui.tabWidget.insertTab(tab_index, self.ui.tab_account, "Account")
+            self.ui_log = self.ui_loged
+            self.ui_log.setupUi(self.ui.tab_account)
+            self.ui_log.LogOutButton.clicked.connect(self.logOut)
+
+            self.ui_log.HelloUserQTextEdit.setText(f"Dear {self.user.name},\nFeel free to browse through our library, "
+                                                   "add books to your library or favourite quotes."
+                                                   " We hope you have a delightful reading experience with BookLingo.")
+
+            self.ui.tabWidget.setCurrentIndex(tab_index)
 
     def logOut(self):
         tab_index=1
@@ -113,6 +160,7 @@ class MainWindow(QMainWindow):
 
 
 if __name__ == "__main__":
+    print()
     app = QApplication()
     window = MainWindow()
     layout = QVBoxLayout()
